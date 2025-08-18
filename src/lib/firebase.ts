6522af72c8eb90b083091e11423df7625867ce0c
rodolVargasdev <rodolfovargasoff@gmail.com>
2025-08-18 15:51:57 -0600

    feat: Configuración multi-entorno para proyecto de staging

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,17 +5,8 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC3zx8GWpHQ3SSlhrZiF4e3kgjGbraEt8g",
-  authDomain: "gestor-licencias-firebas-76c57.firebaseapp.com",
-  projectId: "gestor-licencias-firebas-76c57",
-  storageBucket: "gestor-licencias-firebas-76c57.firebasestorage.app",
-  messagingSenderId: "548549101547",
-  appId: "1:548549101547:web:9682a066fb0dc42c437bae",
-  measurementId: "G-F5YTZ695P3"
-};
+// Importar configuración multi-entorno
+import { firebaseConfig } from './firebase.config';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
